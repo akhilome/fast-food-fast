82aab9bc4a4f07250225b76deb05e0202007a861
Kizito Akhilome <kizito@akhilo.me>
2018-10-04 03:16:05 +0100

    feat(add-food): implement route to allow admin add new meals
    - create a POST /api/v1/menu which can be used by just admins
    - write test for the endpoint
    
    [Finishes #160875996]

--- server/routes/menuRouter.js
+++ server/routes/menuRouter.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import AuthHandler from '../middleware/authHandler';
 import MenuController from '../controllers/menuController';
+import Sanitize from '../middleware/sanitizer';
 
 const router = new Router();
 
 router.get('/', AuthHandler.authorize, MenuController.getMenu);
+router.post(
+  '/',
+  AuthHandler.authorize,
+  AuthHandler.authorizeAdmin,
+  Sanitize.addFood,
+  MenuController.addFood,
+);
 
 export default router;
