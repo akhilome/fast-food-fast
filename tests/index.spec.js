a69ee85327743912f857d3fcfca685355f29d1e6
Kizito Akhilome <kizito@akhilo.me>
2018-10-05 05:28:11 +0100

    fix(unassigned-routes): write failing tests for unassigned routes
    #161008523

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -33,3 +33,32 @@ describe('GET /', () => {
       });
   });
 });
+
+describe('GET /unassigned', () => {
+  it('should respond with a 404 and formatted json', (done) => {
+    chai.request(app)
+      .get('/someunhandledroute')
+      .end((err, res) => {
+        if (err) done(err);
+
+        res.status.should.eql(404);
+        res.body.should.be.an('object');
+        res.body.should.have.keys(['status', 'message']);
+        res.body.message.should.eql('no route has been assigned to that URL');
+        done();
+      });
+  });
+
+  it('should provide a custom error reponse for POSTs too', (done) => {
+    chai.request(app)
+      .post('/nothingishere')
+      .end((err, res) => {
+        if (err) done(err);
+
+        res.status.should.eql(404);
+        res.body.should.be.an('object').which.has.keys(['status', 'message']);
+        res.body.message.should.eql('no route has been assigned to that URL');
+        done();
+      });
+  });
+});
