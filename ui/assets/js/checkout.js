c3180f344da23c2d056a093f6c3be4b99c5dd4d3
Kizito Akhilome <hovkard@gmail.com>
2018-10-13 21:54:17 +0100

    feat(ui-cart): add a cart and initiate checkout flow
    [Finishes #161197342]

---
+++ ui/assets/js/checkout.js
@@ -0,0 +1,41 @@
+function checkoutCardBlueprint(id, foodName, foodPrice) {
+  return `
+  <div data-foodid=${id} class="food-card-checkout">
+    <div class="food-card-checkout__details">
+      <h4>${foodName}</h4>
+      <p>${foodPrice}</p>
+    </div>
+    <button>Remove</button>
+  </div>
+  `;
+}
+
+// Populate Cart Items on Page Load
+(() => {
+  const cartItems = JSON.parse(localStorage.getItem('cart'));
+  if(!Object.keys(cartItems).length) {
+    document.querySelector('#checkout').remove();
+    return document.querySelector('section.checkout').innerHTML = '<div>No items in cart!</div>';
+  }
+
+  const formatted = [];
+  for (const food in cartItems) {
+    formatted.push(checkoutCardBlueprint(food, cartItems[food].foodName, cartItems[food].foodPrice));
+  }
+  document.querySelector('section.checkout').innerHTML = formatted.join('');
+})();
+
+//
+const removeButtons = document.querySelectorAll('.food-card-checkout > button')
+
+function removeFromCart() {
+  const cart = JSON.parse(localStorage.getItem('cart'));
+  const foodCard = this.parentNode;
+  const foodId = foodCard.dataset.foodid;
+  delete cart[foodId];
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  foodCard.remove();
+}
+
+removeButtons.forEach(button => button.addEventListener('click', removeFromCart));
